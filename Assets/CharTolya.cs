c83346c1468738f4b0f8c3a839b06fc3e4d8bbd8
tolyati <tolyakretsu@gmail.com>
2025-10-05 15:05:40 +0300

    Tolya's first commit

--- Assets/CharTolya.cs
+++ Assets/CharTolya.cs
@@ -5,7 +5,7 @@ public class CharTolya : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        int HP = infinity; 
     }
 
     // Update is called once per frame
