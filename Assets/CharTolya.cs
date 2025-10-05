b71b02d62096e0974fcad4ce8bbbd54c455537cd
tolyati <tolyakretsu@gmail.com>
2025-10-05 16:43:00 +0300

    myboles

--- Assets/CharTolya.cs
+++ Assets/CharTolya.cs
@@ -2,6 +2,18 @@ using UnityEngine;
 
 public class CharTolya : MonoBehaviour, IBaseActions
 {
+    public int HP = 70;
+    public int Defense = 30;
+    public int Speed = 100;
+    public int Strength = 0;
+    public int SkillPoint = 0;
+    public int DespairPoint = 0;
+
+    using SP as SkillPoint;
+    using DP as DespairPoint;
+
+
+    
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
