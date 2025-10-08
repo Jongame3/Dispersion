01c7325324c657ce8d7f0093fe2d4cec1cfea1b1
Jongame3 <mrjongame65@gmail.com>
2025-10-08 23:29:42 +0300

    prefab battle UI

--- Assets/RunGame.cs
+++ Assets/RunGame.cs
@@ -1,8 +1,15 @@
+using System.Collections;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class RunGame : MonoBehaviour
 {
     public uint RoundCount = 0;
+    [SerializeField] private CharKsiusha fox;
+    [SerializeField] private CharPoison yad;
+    [SerializeField] private CharNastya fireg;
+    [SerializeField] private Tsumatsu jirai;
+    [SerializeField] private Boss Pasha;
     void Start()
     {
         
